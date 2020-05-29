3a34d28a7355ef8ca6e1500c1231c44f478b78d7
jguerreiro <joel.borghi@gmail.com>
2020-05-29 15:19:28 -0300

    Create Funções_Math.js

---
+++ Funções_Math.js
@@ -0,0 +1,10 @@
+//número absoluto
+Math.abs(10);
+//arredodanmento para +
+Math.round(107.6);
+//arredondamento para -
+Math.floor(50.9);
+//mostrando o maior valor entre os parâmetros
+Math.max(50,200);
+//mostrando o menor valor entre os parâmetro
+Math.min(250,150);
