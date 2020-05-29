6dbabd2f75513f82e46b3a27b4aed1994b05aec0
jguerreiro <joel.borghi@gmail.com>
2020-05-29 15:16:28 -0300

    Create Usando_funções.js

---
+++ Usando_funções.js
@@ -0,0 +1,8 @@
+function triploDaSoma( num1, num2) {
+    
+    var somar = num1 + num2;
+    var resultadoTriplo = triplo(somar);
+    return resultadoTriplo;
+}
+
+var calculo = triploDaSoma(3,2);
