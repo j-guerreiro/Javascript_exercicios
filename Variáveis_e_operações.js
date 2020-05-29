005af0e81357d440d1d3e8d084a4e759955ffa79
jguerreiro <joel.borghi@gmail.com>
2020-05-29 15:10:34 -0300

    Create Variáveis_e_operações.js

---
+++ Variáveis_e_operações.js
@@ -0,0 +1,12 @@
+let numeroA = 2;
+let numeroB = 3;
+let resultadoSoma = ( numeroA + numeroB);
+let resultadoSubtracao = ( numeroA - numeroB);
+let resultadoMultiplicacao = ( numeroA * numeroB);
+let resultadoDivisao = ( numeroA / numeroB);
+
+//printa na tela
+console.log(resultadoSoma);
+console.log(resultadoSubtracao);
+console.log(resultadoMultiplicacao);
+console.log(resultadoDivisao);
