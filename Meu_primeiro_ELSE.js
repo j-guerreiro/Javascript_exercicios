0c9e0404932d5044d9d8ea532a4e85aec237a90e
jguerreiro <joel.borghi@gmail.com>
2020-05-29 15:22:16 -0300

    Create Meu_primeiro_ELSE.js

---
+++ Meu_primeiro_ELSE.js
@@ -0,0 +1,7 @@
+function hojeSeJoga(diaDaSemana){
+    if ( diaDaSemana == 'domingo')
+        return "Hoje é dia de futebol! ! !"
+    else {
+        return "Hoje não é dia de futebol :("
+    }
+}
