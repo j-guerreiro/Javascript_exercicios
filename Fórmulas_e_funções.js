f22a5ab8646e8b64580e5aa77416122a5d82cdfb
jguerreiro <joel.borghi@gmail.com>
2020-05-29 15:17:00 -0300

    Create Fórmulas_e_funções.js

---
+++ Fórmulas_e_funções.js
@@ -0,0 +1,13 @@
+function perimetro( raio ) {
+    
+    return 3.14 * raio * 2;
+};
+
+function area( raio ) {
+
+    return 3.14 * raio * raio ;
+};
+
+
+let resultadoPerimetro = perimetro(3);
+let resultadoArea = area(3);
