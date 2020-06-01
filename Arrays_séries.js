79953f6b31599e31928564c6144981a109beb248
jguerreiro <joel.borghi@gmail.com>
2020-06-01 16:56:18 -0300

    Create Arrays_séries.js

---
+++ Arrays_séries.js
@@ -0,0 +1,10 @@
+var seriesFavoritas1 = 
+["Game of Thrones", "Breaking Bad", "House of Cards"];
+var seriesFavoritas2 = ["Blindspot", "Blacklist"];
+var msg = ["olá", "mundo!"];
+var msg2 = ["olá", "olá"];
+
+console.log(seriesFavoritas1);
+console.log(seriesFavoritas2);
+console.log(msg);
+console.log(msg2);
