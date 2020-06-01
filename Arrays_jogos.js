5f4334537a83ebf591c89a3ad14ab2866a82d46e
jguerreiro <joel.borghi@gmail.com>
2020-06-01 16:58:04 -0300

    Create Arrays_jogos.js

---
+++ Arrays_jogos.js
@@ -0,0 +1,9 @@
+var numerosDeLoteria = [2, 11, 17, 32, 36, 39];
+var girosDeDado = [1, 6, 6, 2, 2, 4];
+var saiuCara = [false, false, true, false];
+var listaDeNumeros = [[1, 2, 3], [4, 5, 6]];
+
+console.log(numerosDeLoteria);
+console.log(girosDeDado);
+console.log(saiuCara);
+console.log(listaDeNumeros);
