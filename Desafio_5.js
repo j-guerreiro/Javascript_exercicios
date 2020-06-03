661b5af6ee1142429528e75da77c42f75e541978
jguerreiro <joel.borghi@gmail.com>
2020-06-03 13:32:53 -0300

    Update Desafio_5.js

--- Desafio_5.js
+++ Desafio_5.js
@@ -2,10 +2,10 @@ function escada ( altura ) {
     var cont = 0;
     var umArray= [ ];
 
-    for ( let i = 0 ; i < altura ; i++ ) {
-        
+    for ( let i = 1 ; i <= altura ; i++ ) {
+        let vazio = altura - i;
         cont += 1;
-        umArray.push( '#'.repeat( cont ) );
+        umArray.push( " ".repeat( vazio ) + "#".repeat( cont ));
 
     }
     return umArray;
