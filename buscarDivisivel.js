cce6733b9d68c9ea42413291d2b9fc21ae9e2410
jguerreiro <joel.borghi@gmail.com>
2020-07-26 11:33:12 -0300

    Add files via upload

---
+++ buscarDivisivel.js
@@ -0,0 +1,17 @@
+function buscarDivisivelPor(array, num) {
+  // Escreva abaixo o seu código:
+
+  for ( let i = 0 ; i < array.length ; i++ ) {
+    if ( ( array[i] != 0 ) && ( (array[i] % num ) == 0 ) ) {
+      let encontrado = array[i];
+      return "Número encontrado! O número: " + encontrado + " é divisível por " + num + " !";
+      break;
+    } 
+  }
+  return "Nenhum número encontrado";
+}
+
+vetor = [0, 9, 4, 7, 128, 42, -1, 301, -5];
+numero = 8;
+
+console.log(buscarDivisivelPor(vetor,numero)); 
