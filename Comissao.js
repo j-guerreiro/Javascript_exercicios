3fee0c270b61d69bfd326c4d9ae05d3fa1b3c90f
jguerreiro <joel.borghi@gmail.com>
2020-07-26 11:35:16 -0300

    Create Comissao.js

---
+++ Comissao.js
@@ -0,0 +1,8 @@
+/*
+
+Crie um sistema para calcular a comissão dos vendedores de uma loja. Crie uma função que tenha dois argumentos: preço e porcentagem de comissão.
+
+*/
+function comissao(preco,porcentagem){
+   return preco * ( porcentagem / 100);
+}
