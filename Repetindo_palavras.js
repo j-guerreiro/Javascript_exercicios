a5043d3a59265badf13699013b20d03b26a4bba0
jguerreiro <joel.borghi@gmail.com>
2020-07-26 11:37:38 -0300

    Create Repetindo_palavras.js

---
+++ Repetindo_palavras.js
@@ -0,0 +1,14 @@
+/*
+
+Crie uma função que receba dois valores, 
+o primeiro será uma palavra qualquer, 
+e o segundo será a quantidade de vezes que o sistema irá imprimir essa palavra.
+
+*/
+
+function repete(palavra,qtd){
+
+    for ( let i = 0 ; i < qtd ; i++) {
+        console.log(palavra);
+    }
+}
