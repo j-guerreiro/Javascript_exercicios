6316b67b3b1dfc586887af3bfad37e9110b852a2
jguerreiro <joel.borghi@gmail.com>
2020-07-26 11:36:14 -0300

    Update buscarDivisivel.js

--- buscarDivisivel.js
+++ buscarDivisivel.js
@@ -1,3 +1,12 @@
+/*
+
+Buscar divisível por
+Programe uma função buscarDivisivelPor que recebe dois parâmetros, 
+um array de números e um número de teste, 
+retornando como resposta o primeiro número do array que seja divisível pelo número dado e diferente de zero. 
+Caso nenhum número do array passe no teste, retorne o texto "Nenhum número válido encontrado!"
+
+*/
 function buscarDivisivelPor(array, num) {
 
   for ( let i = 0 ; i < array.length ; i++ ) {
